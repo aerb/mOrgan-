8774b1a2d21b5bb5cbe9f5b144ddff65e06e860a
Adam <adam.l.erb@gmail.com>
2011-06-25 11:32:27 -0700

    Finally got the arguement parser working how it is supposed to.

--- src/main.py
+++ src/main.py
@@ -280,29 +280,39 @@ def print_final_stats():
     o_file.write("# Potential Memory Freed: %f Mb\n" % (float(total_bytes)/1000000))
 
 
+def were_all_done_here():
+    o_file.close()
+    log_file.close()
+
 def parse_command_line():
     ns = c_parser.parse_args(''.split())
+    print ns
     if len(sys.argv) == 1:
         c_parser.print_help()
 
     opt = vars(ns)
-    print opt    
     
-    if opt['evaluate'] == None:
-        Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-        filename = askdirectory() # show an "Open" dialog box and return the path to the selected file
-   
-#    find_musics(music_root_dir)
+    if opt['evaluate'] != 'NADA':
+        if opt['evaluate'] == None:
+            Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
+            filename = askdirectory() # show an "Open" dialog box and return the path to the selected file
+        else:
+            filename = opt['evaluate']
+        
+        find_musics(music_root_dir)
     
-    o_file.close()
-    log_file.close()
-    #delete_stuff(False)
+    if opt['delete'] == 'delete':
+        delete_stuff(True)
+    elif opt['delete'] == 'purgatory':
+        delete_stuff(False)
+    
+    were_all_done_here()
 
 
 c_parser = argparse.ArgumentParser(description = 'A simple music duplicates eliminator and Music ORGANizer.',
                                    epilog = 'sihosgih')
 
-c_parser.add_argument('--evaluate','-e', action='store_true' ,help = 'Define the root directory to evaluate duplicates. If no directory is given a folder prompt will be presented later.')
+c_parser.add_argument('--evaluate','-e', metavar='Music-Directory', nargs = '?',default='NADA' ,help = 'Define the root directory to evaluate duplicates. If no directory is given a folder prompt will be presented later.')
 
 c_parser.add_argument('--delete', '-d', choices = ['trash','purgatory'], help = 'Option tells mOrgan to remove one item for each conflict from the music directory.\n If trash is specified items will be sent trash/recycling bin. If purgatory is specified files will be moved to local folder in working directory which can then be reviewed before manual deletion.')
 c_parser.add_argument('--log', '-l', action='store_true', help = 'Specifies if you would like a log file for debugging purposes.')
