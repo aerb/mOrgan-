34b8622bdb56e04ef648c412e764010620a576ee
Adam <adam.l.erb@gmail.com>
2011-07-05 08:33:43 -0700

    GUI with some events handling added.

--- src/mOrgan_gui.py
+++ src/mOrgan_gui.py
@@ -1,47 +1,118 @@
-import wx
-from wx import *
-
+import wx 
+import sys
 import mOrgan_lib
-from xml.etree.ElementTree import *
+from xml.etree.ElementTree import Element, dump, SubElement, ElementTree,parse
 
-class MyApp(wx.App):
-    def OnInit(self):
-        frame = wx.Frame(None, -1, "Hello from wxPython",( -1,-1),(800,400))
+class Example(wx.Frame):
+  
+    def __init__(self, parent, title):
+        super(Example, self).__init__(parent, title=title, size=(800, 600))
+                    
+        self.InitUI()
+        self.Centre()
+        self.Show()     
+        
+    def InitUI(self):
+    
+        panel = wx.Panel(self)
         
 
+        hbox = wx.BoxSizer(wx.HORIZONTAL)
+
+        fgs = wx.FlexGridSizer(4, 1, 9, 25)
+
+        term_text = wx.StaticText(panel, label="Terminal")
+        conf_text = wx.StaticText(panel, label="Conflicting Items")
+        
         id=wx.NewId()
-        self.list=wx.ListCtrl(frame,id,style=wx.LC_REPORT|wx.SUNKEN_BORDER)
-        self.list.Show(True)
+        list=wx.ListCtrl(panel,id,style=wx.LC_REPORT|wx.SUNKEN_BORDER)
 
-        self.list.InsertColumn(0,"Delete?")
-        self.list.InsertColumn(1,"Filename")
-        self.list.InsertColumn(2,"length")
-        self.list.InsertColumn(3,"size")
-        self.list.InsertColumn(4,"sample rate")
-        self.list.InsertColumn(5,"bitrate")
+        list.InsertColumn(0,"Delete?")
+        list.InsertColumn(1,"Filename")
+        list.InsertColumn(2,"length")
+        list.InsertColumn(3,"size")
+        list.InsertColumn(4,"sample rate")
+        list.InsertColumn(5,"bitrate")
         
-        self.list.Bind( wx.EVT_LIST_ITEM_ACTIVATED, self.list_d_click )
-
+#        list.Bind( wx.EVT_LIST_ITEM_ACTIVATED, self.list_d_click )
+#
         tree = parse("conflicts.xml")
         elem = tree.getroot()
         
         for i in tree.getiterator('conflict'):
             for j in i:
-                pos = self.list.InsertStringItem(0, "Yes")
-                self.list.SetStringItem(pos,1, j.get('location'))
-                self.list.SetStringItem(pos,2, j.get('length'))
-                self.list.SetStringItem(pos,3, j.get('size'))
-                self.list.SetStringItem(pos,4, j.get('sample_rate'))
-                self.list.SetStringItem(pos,5, j.get('bitrate'))
-        
-        frame.Show(True)
-        self.SetTopWindow(frame)
-        return True
-    
-    def list_d_click( self, event ):
-        print "double click"
-        index = event.GetIndex()
-        self.list.SetStringItem(index,0, "No")
+                pos = list.InsertStringItem(0, "Yes")
+                list.SetStringItem(pos,1, j.get('location'))
+                list.SetStringItem(pos,2, j.get('length'))
+                list.SetStringItem(pos,3, j.get('size'))
+                list.SetStringItem(pos,4, j.get('sample_rate'))
+                list.SetStringItem(pos,5, j.get('bitrate'))
+                
+        term_box = wx.TextCtrl(panel, wx.ID_ANY, size=(300,100),style = wx.TE_MULTILINE|wx.TE_READONLY|wx.HSCROLL)
 
-app = MyApp(0)
-app.MainLoop()
+        redir=RedirectText(term_box)
+        sys.stdout=redir
+        
+        print 'test'
+        print 'not'
+
+        fgs.AddMany([(conf_text),(list,1,wx.EXPAND),(term_text),(term_box,1,wx.EXPAND)])
+        
+        fgs.AddGrowableCol(0, 1)
+        fgs.AddGrowableRow(1, 1)
+
+        hbox.Add(fgs, proportion=1, flag=wx.ALL|wx.EXPAND, border=15)
+        panel.SetSizer(hbox)
+
+        menuBar = wx.MenuBar()
+        menu = wx.Menu()
+        
+        nmorg = menu.Append(-1, "Search")
+        self.Bind(wx.EVT_MENU, self.OnNew, nmorg)
+        
+        m_exit = menu.Append(wx.ID_EXIT, "E&xit\tAlt-X", "Close window and exit program.")
+        self.Bind(wx.EVT_MENU, self.OnClose, m_exit)
+
+        menuBar.Append(menu, "&File")
+        
+        menu = wx.Menu()
+        m_about = menu.Append(wx.ID_ABOUT, "&About", "Information about this program")
+        self.Bind(wx.EVT_MENU, self.OnAbout, m_about)
+        menuBar.Append(menu, "&Help")
+        self.SetMenuBar(menuBar)
+        
+        
+    def OnClose(self, event):
+        dlg = wx.MessageDialog(self, 
+            "Do you really want to close this application?",
+            "Confirm Exit", wx.OK|wx.CANCEL|wx.ICON_QUESTION)
+        result = dlg.ShowModal()
+        dlg.Destroy()
+        if result == wx.ID_OK:
+            self.Destroy()
+    
+    def OnNew(self,event):
+        dialog = wx.DirDialog (self, "pick a folder..")
+        if dialog.ShowModal() == wx.ID_OK:
+            print dialog.GetPath()
+            ml = mOrgan_lib.mOrgan_lib(dialog.GetPath())
+            ml.start()
+        dialog.Destroy() 
+    
+    def OnAbout(self, event):
+        dlg = AboutBox()
+        dlg.ShowModal()
+        dlg.Destroy() 
+
+
+class RedirectText(object):
+    def __init__(self,aWxTextCtrl):
+        self.out=aWxTextCtrl
+ 
+    def write(self,string):
+        self.out.WriteText(string)
+
+if __name__ == '__main__':
+        app = wx.App(0)
+        Example(None, title='Review')
+        app.MainLoop()
--- src/mOrgan_lib.py
+++ src/mOrgan_lib.py
@@ -10,6 +10,7 @@ import traceback
 import math
 import fileinput
 import shutil
+import threading
 
 import subprocess
 from xml.etree.ElementTree import Element, dump, SubElement, ElementTree
@@ -31,20 +32,15 @@ import os
 from os import listdir
 from os.path import join
 
+
 output_path = "conflict_report.m3u"
 log_path = "debugging.log"
 xml_root = Element("root")
 
-
 o_file = open(output_path,'w')
 log_file = open(log_path,'w')
-#music_root_dir = 'C:\Users\Adam\Documents\\test_dir'
 delete_path = 'delete_me'
-#music_root_dir = "C:\Users\Adam\Music"
-
-music_root_dir = 'C:\\test_folder_music'
-
-morgan_library = {}
+music_root_dir = ''
 
 conflict_count = 0
 total_bytes = 0
@@ -59,231 +55,252 @@ dash_regex = re.compile('(?<=[A-Za-z0-9])\-(?=[A-Za-z0-9])')
 space_regex = re.compile('[ \t]+')
 to_delete_regex = re.compile('\"[^\"\r\n]*\"')
 
-def delete_stuff(really_delete):
-    try:
-        os.mkdir(delete_path)
-    except OSError:
-        pass
+class mOrgan_lib(threading.Thread):
     
-    for line in fileinput.input(output_path):
-        if "#> Delete" in line:
-            try:
-                found =  to_delete_regex.search(line)
-                src = found.group()
-                src = src.replace('\"','')
-            except:
-                raise
-                
-            print "deleting file:", src
-            try:
-                if really_delete:
-                    send2trash.send2trash(src)
-                else:                
-                    shutil.move(src, join(delete_path,os.path.basename(src)))
-            except:
-                print "Error: corrupt delete file."
-                traceback.print_exc(file=sys.stdout)   
-
-def find_musics(path):
-    for i in listdir(path):
-        if os.path.isdir(join(path, i)):
-            find_musics(join(path, i))
-        else:
-            try:
-                are_you_musics(join(path, i))
-            except mutagen.id3.ID3NoHeaderError:
-                log_file.write("ID3NoHeaderError on: %s\n\n" % join(path, i))
-            except KeyError:
-                log_file.write("Key Error on: %s\n\n" % join(path, i))
-                traceback.print_exc(file=log_file)
-            except IOError:
-                log_file.write("IO Error on: %s\n\n" % join(path, i))
-            except:
-                log_file.write("Unexpected error: %s\n" % join(path, i))
-                log_file.write("%s\n" % str(sys.exc_info()))
-                traceback.print_exc(file=log_file)
-                log_file.write("\n")
-
-            
-def normalize_track_data(art,alb,name):
-    art = normalize_strings(art)
-    art = the_regex.sub('',art)
-    alb = normalize_strings(alb)
-    name = normalize_strings(name)
-    return art,alb,name
-            
-def normalize_strings(s):
-    s = string.lower(s)             #convert to lower
-    s = dash_regex.sub(' ',s)       #replace all joining dashes with space
-    s = and_regex.sub(' and ',s)    #replace & with and
-    s = punc_regex.sub('',s)        #strip all puncuation
-    s = space_regex.sub('',s)       #remove redundant whitepsace (2+ spaces in row) 
-    return s
-            
-def are_you_musics(item):
-    
-    basename, ext = os.path.splitext(item)
-    
-    if ".m4a" == ext:
-        f = M4A(item)
-        artist,album,title = normalize_track_data(f['\xa9ART'],
-                                                  f['\xa9alb'],
-                                                  f['\xa9nam'])
-    elif ".mp3" in item:
-        f = EasyID3(item)
-        artist,album,title = normalize_track_data(f['artist'][0],
-                                                  f['album'][0],
-                                                  f['title'][0])            
-    elif ".mp4" == ext or ".m4p" == ext:
-        f = EasyMP4(item)
-        artist,album,title = normalize_track_data(f['artist'][0],
-                                                  f['album'][0],
-                                                  f['title'][0])
-    elif ".flac" == ext:
-        f = FLAC(item)
-        artist,album,title = normalize_track_data(f['artist'][0],
-                                                  f['album'][0],
-                                                  f['title'][0])
-    elif ".wma" == ext or ".wmv" == ext:
-        f = ASF(item)
-        artist,album,title = normalize_track_data(str(f['WM/AlbumArtist'][0]),
-                                                  str(f['WM/AlbumTitle'][0]),
-                                                  str(f['Title'][0]))
-    elif ".ogg" == ext:
-        f = OggVorbis(item)
-        artist,album,title = normalize_track_data(f['artist'][0],
-                                                  f['album'][0],
-                                                  f['title'][0])
-    else:
-        return
-
-    if artist not in morgan_library:
-        morgan_library[artist] = {}
-        print "Indexing artist: " , artist
-    if  album not in morgan_library[artist]:
-        morgan_library[artist][album] = {}
-    if title not in morgan_library[artist][album]:
-        morgan_library[artist][album][title] = item
-        if not quiet:
-            log_file.write("%s : %s : %s\n" % (artist, album, title))
-    else:
-        if evaluate_length(morgan_library[artist][album][title], item):
-            evaluate_conflicting_items(morgan_library[artist][album][title], item)
-
-def evaluate_length(item1,item2):
-    f1 = mutagen.File(item1)
-    f2 = mutagen.File(item2)
-    return 5 > math.fabs(f1.info.length - f2.info.length)
-
-def item_info(item,type):
-    try:
-        f = mutagen.File(item)
-        if type is 'length':
-            return f.info.length
-        elif type is 'size':
-            return os.path.getsize(item)
-        elif type is 'bitrate':
-            return f.info.bitrate
-        elif type is 'sample_rate':
-            return f.info.sample_rate
-    except IOError:
-        return None
-            
-def item_info_s(item,type):
-    return str(item_info(item,type))
-
-def evaluate_conflicting_items(item1, item2):
-    global conflict_count
-    global total_bytes
-    
-    conflict_count = conflict_count + 1
-    
-    xml_conflict = SubElement(xml_root, 'conflict')
-    SubElement(xml_conflict, 'conflict_item',id = '1', location = item1, length = item_info_s(item1,'length'),size = item_info_s(item1,'size'),sample_rate=item_info_s(item1,'sample_rate'),bitrate=item_info_s(item1,'bitrate'))
-    SubElement(xml_conflict, 'conflict_item',id = '2', location = item2, length = item_info_s(item2,'length'),size = item_info_s(item2,'size'),sample_rate=item_info_s(item2,'sample_rate'),bitrate=item_info_s(item2,'bitrate'))
-    
-    resolved = None
-    resolved = evaluate_by_sample_rate(item1, item2)
-    if resolved == None:
-        resolved = evaluate_by_bit_rate(item1,item2)
-    if resolved == None:
-        resolved = evaluate_by_size(item1,item2)
-    if resolved == None:
-        resolved = evaluate_by_string_length(item1,item2)
-    if resolved == None:
-        resolved = item2
-    
-    xml_conflict.attrib['resolved']= resolved
-    
-    #TODO: Add define xml_conflict attributes for deletion 
-    
-    total_bytes = total_bytes + os.path.getsize(resolved)
-
-def evaluate_by_string_length(item1,item2):
-    #silly, but if files identical delete one with longer string length
-    #this will eliminate "Copy of song.mp3" vs. "song.mp3"
-    
-    if len(item1) > len(item2):
-        return item1
-    elif len(item1) < len(item2):
-        return item2
-    else:
-        return None
-
-def evaluate_by_bit_rate(item1,item2):
-    f1 = mutagen.File(item1)
-    f2 = mutagen.File(item2)
-    
-    try:
-        if f1.info.bitrate > f2.info.bitrate:
-            return item2
-        elif f1.info.bitrate < f2.info.bitrate:
-            return item1
-        else :
-            return None
-    except:
-        return None
-
-def evaluate_by_size(item1,item2):
-    f1 = os.path.getsize(item1)
-    f2 = os.path.getsize(item2)
-    try:
+    def __init__(self,music_dir):
+        threading.Thread.__init__(self)
+        self.music_root_dir = music_dir
+        self.morgan_library = {}
         
-        if f1 > f2:
-            return item2
-        elif f1 < f2:
+    
+    def delete_stuff(self,really_delete):
+        try:
+            os.mkdir(delete_path)
+        except OSError:
+            pass
+        
+        for line in fileinput.input(output_path):
+            if "#> Delete" in line:
+                try:
+                    found =  to_delete_regex.search(line)
+                    src = found.group()
+                    src = src.replace('\"','')
+                except:
+                    raise
+                    
+                print "deleting file:", src
+                try:
+                    if really_delete:
+                        send2trash.send2trash(src)
+                    else:                
+                        shutil.move(src, join(delete_path,os.path.basename(src)))
+                except:
+                    print "Error: corrupt delete file."
+                    traceback.print_exc(file=sys.stdout)   
+    
+    def find_musics(self,path):
+        for i in listdir(path):
+#            print join(path, i)
+            if os.path.isdir(join(path, i)):
+                self.find_musics(join(path, i))
+            else:
+                try:
+                    self.are_you_musics(join(path, i))
+                except mutagen.id3.ID3NoHeaderError:
+                    log_file.write("ID3NoHeaderError on: %s\n\n" % join(path, i))
+                except KeyError:
+                    log_file.write("Key Error on: %s\n\n" % join(path, i))
+                except IOError:
+                    log_file.write("IO Error on: %s\n\n" % join(path, i))
+                except:
+                    to_write = sys.stdout
+                    to_write.write("Unexpected error: %s\n" % join(path, i))
+                    to_write.write("%s\n" % str(sys.exc_info()))
+                    traceback.print_exc(file=to_write)
+                    to_write.write("\n")
+    
+                
+    def normalize_track_data(self,art,alb,name):
+        art = self.normalize_strings(art)
+        art = the_regex.sub('',art)
+        alb = self.normalize_strings(alb)
+        name = self.normalize_strings(name)
+        return art,alb,name
+                
+    def normalize_strings(self,s):
+        s = string.lower(s)             #convert to lower
+        s = dash_regex.sub(' ',s)       #replace all joining dashes with space
+        s = and_regex.sub(' and ',s)    #replace & with and
+        s = punc_regex.sub('',s)        #strip all puncuation
+        s = space_regex.sub('',s)       #remove redundant whitepsace (2+ spaces in row) 
+        return s
+    
+                
+    def are_you_musics(self,item):
+        
+        basename, ext = os.path.splitext(item)
+        
+        if ".m4a" == ext:
+            f = M4A(item)
+            artist,album,title = self.normalize_track_data(f['\xa9ART'],
+                                                      f['\xa9alb'],
+                                                      f['\xa9nam'])
+        elif ".mp3" in item:
+            f = EasyID3(item)
+            artist,album,title = self.normalize_track_data(f['artist'][0],
+                                                      f['album'][0],
+                                                      f['title'][0])            
+        elif ".mp4" == ext or ".m4p" == ext:
+            f = EasyMP4(item)
+            artist,album,title = self.normalize_track_data(f['artist'][0],
+                                                      f['album'][0],
+                                                      f['title'][0])
+        elif ".flac" == ext:
+            f = FLAC(item)
+            artist,album,title = self.normalize_track_data(f['artist'][0],
+                                                      f['album'][0],
+                                                      f['title'][0])
+        elif ".wma" == ext or ".wmv" == ext:
+            f = ASF(item)
+            artist,album,title = self.normalize_track_data(str(f['WM/AlbumArtist'][0]),
+                                                      str(f['WM/AlbumTitle'][0]),
+                                                      str(f['Title'][0]))
+        elif ".ogg" == ext:
+            f = OggVorbis(item)
+            artist,album,title = self.normalize_track_data(f['artist'][0],
+                                                      f['album'][0],
+                                                      f['title'][0])
+        else:
+            return
+    
+        if artist not in self.morgan_library:
+            self.morgan_library[artist] = {}
+            print "Indexing artist: " , artist
+        if  album not in self.morgan_library[artist]:
+            self.morgan_library[artist][album] = {}
+        if title not in self.morgan_library[artist][album]:
+            self.morgan_library[artist][album][title] = item
+            if not quiet:
+                log_file.write("%s : %s : %s\n" % (artist, album, title))
+        else:
+            if self.evaluate_length(self.morgan_library[artist][album][title], item):
+                self.evaluate_conflicting_items(self.morgan_library[artist][album][title], item)
+    
+    def evaluate_length(self,item1,item2):
+        f1 = mutagen.File(item1)
+        f2 = mutagen.File(item2)
+        return 5 > math.fabs(f1.info.length - f2.info.length)
+    
+    def item_info(self,item,type):
+        try:
+            f = mutagen.File(item)
+            if type is 'length':
+                return f.info.length
+            elif type is 'size':
+                return os.path.getsize(item)
+            elif type is 'bitrate':
+                return f.info.bitrate
+            elif type is 'sample_rate':
+                return f.info.sample_rate
+        except IOError:
+            return None
+                
+    def item_info_s(self,item,type):
+        return str(self.item_info(item,type))
+    
+    def evaluate_conflicting_items(self,item1, item2):
+        global conflict_count
+        global total_bytes
+        
+        conflict_count = conflict_count + 1
+        
+        xml_conflict = SubElement(xml_root, 'conflict')
+        SubElement(xml_conflict, 'conflict_item',id = '1', location = item1, length = self.item_info_s(item1,'length'),size = self.item_info_s(item1,'size'),sample_rate=self.item_info_s(item1,'sample_rate'),bitrate=self.item_info_s(item1,'bitrate'))
+        SubElement(xml_conflict, 'conflict_item',id = '2', location = item2, length = self.item_info_s(item2,'length'),size = self.item_info_s(item2,'size'),sample_rate=self.item_info_s(item2,'sample_rate'),bitrate=self.item_info_s(item2,'bitrate'))
+        
+        resolved = None
+        resolved = self.evaluate_by_sample_rate(item1, item2)
+        if resolved == None:
+            resolved = self.evaluate_by_bit_rate(item1,item2)
+        if resolved == None:
+            resolved = self.evaluate_by_size(item1,item2)
+        if resolved == None:
+            resolved = self.evaluate_by_string_length(item1,item2)
+        if resolved == None:
+            resolved = item2
+        
+        xml_conflict.attrib['resolved']= resolved
+        
+        #TODO: Add define xml_conflict attributes for deletion 
+        
+        total_bytes = total_bytes + os.path.getsize(resolved)
+    
+    def evaluate_by_string_length(self,item1,item2):
+        #silly, but if files identical delete one with longer string length
+        #this will eliminate "Copy of song.mp3" vs. "song.mp3"
+        
+        if len(item1) > len(item2):
             return item1
+        elif len(item1) < len(item2):
+            return item2
         else:
             return None
-    except:
-        return None
-
-def evaluate_by_sample_rate(item1, item2):
-    f1 = mutagen.File(item1)
-    f2 = mutagen.File(item2)
-    try:
-
-        if f1.info.sample_rate > f2.info.sample_rate:
-            return item2
-        elif f1.info.sample_rate < f2.info.sample_rate:
-            return item1
-        else:
+    
+    def evaluate_by_bit_rate(self,item1,item2):
+        f1 = mutagen.File(item1)
+        f2 = mutagen.File(item2)
+        
+        try:
+            if f1.info.bitrate > f2.info.bitrate:
+                return item2
+            elif f1.info.bitrate < f2.info.bitrate:
+                return item1
+            else :
+                return None
+        except:
             return None
-    except:
-        return None
+    
+    def evaluate_by_size(self,item1,item2):
+        f1 = os.path.getsize(item1)
+        f2 = os.path.getsize(item2)
+        try:
+            
+            if f1 > f2:
+                return item2
+            elif f1 < f2:
+                return item1
+            else:
+                return None
+        except:
+            return None
+    
+    def evaluate_by_sample_rate(self,item1, item2):
+        f1 = mutagen.File(item1)
+        f2 = mutagen.File(item2)
+        try:
+    
+            if f1.info.sample_rate > f2.info.sample_rate:
+                return item2
+            elif f1.info.sample_rate < f2.info.sample_rate:
+                return item1
+            else:
+                return None
+        except:
+            return None
+    
+    
+    def were_all_done_here(self):
+        o_file.close()
+        log_file.close()
+    
+    def open_playlist(self):
+        if os.name == 'mac':
+            subprocess.call(('open', output_path))
+        elif os.name == 'nt':
+            subprocess.call(('start', output_path), shell=True)
+        elif os.name == 'posix':
+            subprocess.call(('xdg-open', output_path))
+    
+    def write_xml(self) :
+        ElementTree(xml_root).write("conflicts.xml")
+    
+    def run(self):
+        print 'lib ready'
+        dir = self.music_root_dir
+        self.find_musics(dir)
+        self.write_xml()
 
 
-def were_all_done_here():
-    o_file.close()
-    log_file.close()
-
-def open_playlist():
-    if os.name == 'mac':
-        subprocess.call(('open', output_path))
-    elif os.name == 'nt':
-        subprocess.call(('start', output_path), shell=True)
-    elif os.name == 'posix':
-        subprocess.call(('xdg-open', output_path))
-
-def write_xml() :
-    ElementTree(xml_root).write("conflicts.xml")
+#print 'hello'
+#ml = mOrgan_lib('C:\Users\Adam\Music')
+#ml.find_musics('C:\Users\Adam\Music')
